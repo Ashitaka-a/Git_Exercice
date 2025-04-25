26436f7ae24a85296cb7bc73ee71951ec9ea2fe6
Adrien <adrien-simon@outlook.com>
2025-04-25 16:01:38 +0200

    exo 25

---
+++ exercice-25-04/script.js
@@ -0,0 +1,23 @@
+let prenom = prompt("Quel est ton nom et prénom ?");
+let age = parseInt(prompt("Quel est ton âge ?"));
+let estEtudiant = prompt("Es-tu étudiant ? (oui/non)").toLowerCase() === "oui";
+let nombreTickets = parseInt(prompt("Combien de tickets veux-tu acheter ?"));
+let reduction;
+const prixBase = 12;
+
+console.log(prenom);
+console.log(age);
+console.log(estEtudiant);
+console.log(nombreTickets);
+console.log(reduction);
+console.log(prixBase);
+
+if (age < 12) {
+    reduction = 0.5;
+} else if (age > 60) {
+    reduction = 0,3;
+} else if (estEtudiant) {
+    reduction = 0,2;
+} else {
+    reduction = 0;
+}
