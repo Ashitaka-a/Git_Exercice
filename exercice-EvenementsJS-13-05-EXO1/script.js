1d568aa09eab4f9c667761819185915aa4ac1853
Adrien <adrien-simon@outlook.com>
2025-05-14 09:58:27 +0200

    exercice 1 événement JS terminé

--- exercice-EvenementsJS-13-05-EXO1/script.js
+++ exercice-EvenementsJS-13-05-EXO1/script.js
@@ -42,4 +42,22 @@ bouton.addEventListener("click", function() {
 
 // Partie 6 // 
 
+const bouttonAjoutDiv = document.getElementById("ajoutDiv");
+const divContainer = document.getElementById("container");
 
+// dis à JS, quand on CLICK sur le bouton execute la fonction qui suit, => veut dire qu'elle sera executé au moment du clique //
+bouttonAjoutDiv.addEventListener("click", () => {
+  const p = document.createElement("p"); // on créer un nouveau élément "p" // 
+  p.textContent = "Nouveau paragraphe crée !"; // on défini le texte à l'intérieur du "p" qui est juste au dessus //
+  divContainer.appendChild(p); // ici on ajoute le "p" dans une div, on a crée la const juste au dessus qui séléctionne la div dans le htmk // 
+
+});
+
+// Défi 7 // 
+
+const suppprimerDiv = document.getElementById("containerRemove");
+
+suppprimerDiv.addEventListener("click", () => {
+  suppprimerDiv.remove(); // le remove() supprime l'élément du DOM directement //
+
+});
